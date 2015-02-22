63a3357337c15bf1a94539bcec7c05986fe9b96d
Lenny Urbanowski <lenny@itslennysfault.com>
2015-02-21 16:36:56 -0800

    build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#creates minified version and source map
+#requires uglifyjs -- npm install uglifyjs -g
+
+PROJECT_NAME=angular-pretty-checkable
+
+uglifyjs ./src/${PROJECT_NAME}.js --comments --source-map ./dist/${PROJECT_NAME}.min.js.map --source-map-url ${PROJECT_NAME}.min.js.map --output ./dist/${PROJECT_NAME}.min.js
